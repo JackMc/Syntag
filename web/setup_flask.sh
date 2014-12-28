962b6e2dc15dac669fd0c5de7d69041fcc3f8069
Jack McCracken <jackamc19@gmail.com>
2014-12-28 15:39:23 -0500

    Add some new helper scripts

---
+++ web/setup_flask.sh
@@ -0,0 +1,16 @@
+## Sets up Flask for a new project
+## (In the current directory)
+python -m venv venv
+source venv/bin/activate
+pip install flask
+pip install flask-login
+pip install flask-openid
+pip install flask-mail
+pip install flask-sqlalchemy
+pip install sqlalchemy-migrate
+pip install flask-whooshalchemy
+pip install flask-wtf
+pip install flask-babel
+pip install guess_language
+pip install flipflop
+pip install coverage
